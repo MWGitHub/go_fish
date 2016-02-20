c2719dd04e4f9aed5b52563daf4f8a3cfe14d4b6
Michael Wang <mwwpublic@gmail.com>
2016-02-20 17:28:57 -0500

    Add deck solution and loading from solution files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,13 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# Load from the solutions directory instead of the lib directory.
+# Comment this out to load from the lib directory.
+lib_index = $LOAD_PATH.index { |v| v.match('go_fish/lib') }
+path = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH[lib_index] = path + '/../solution'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
